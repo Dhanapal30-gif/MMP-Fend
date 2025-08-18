d566609e6345ade237f5d7ca31ba74bfd81d3320
Dhanapal30-gif <dhanapal05v@gmail.com>
2025-08-18 18:29:18 +0530

    install npm install @fontsource/roboto

--- MMP-vite/src/main.jsx
+++ MMP-vite/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';          // ✅ add this
-import '@fontsource/roboto';   // ✅ add this if using MUI
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
